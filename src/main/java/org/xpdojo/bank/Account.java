b986368c80ec9c4d494c3b414d666cd4d244521a
Iulia Tudor <iulia.tomescu@gmail.com>
2022-07-15 15:01:09 +0300

    Initial deposit test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int amount=0;
+
+    public void deposit(int i) {
+        amount = amount +i;
+    }
+
+    public int balance() {
+    return amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+//import static org.hamcrest.MatcherAssert.AssertThat;
+//import static org.hamcrest.Matchers.*;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+	public void depositAnAmountToIncreaseTheBalance() {
+        Account account = new Account();
+        assertThat(account.balance()==0);
+        account.deposit(100);
+        assertThat(account.balance()==100);
     }
 }
